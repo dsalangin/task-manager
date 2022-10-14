5c975281a69b94b90c48aba2222ace3d589e572f
dsalangin <dsalangin@74.ru>
2022-10-14 11:42:16 +0300

    Переписал компонент меню через наследование от абстрактного компонента

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,4 @@
-import {createElement} from '../utils';
+import AbstractComponent from './abstract-component';
 
 const createMenuTemplate = () => {
   return (
@@ -34,26 +34,12 @@ const createMenuTemplate = () => {
 };
 
 
-class SiteMenuComponent {
-  constructor() {
-    this._element = null;
-  }
+class SiteMenuComponent extends AbstractComponent {
 
   getTemplate() {
     return createMenuTemplate();
   }
 
-  getElement() {
-    if (!this._element) {
-      this._element = createElement(this.getTemplate());
-    }
-
-    return this._element;
-  }
-
-  removeElement() {
-    this._element = null;
-  }
 }
 
 export default SiteMenuComponent;
