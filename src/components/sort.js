f80560511c6ff704b9bd97cd6c85e1d89787c445
dsalangin <dsalangin@74.ru>
2022-10-19 17:47:25 +0300

    Добавил сортировке обработку события на клик

--- src/components/sort.js
+++ src/components/sort.js
@@ -1,11 +1,17 @@
 import AbstractComponent from "./abstract-component";
 
+const SortType = {
+  DEFAULT: `default`,
+  DATE_DOWN: `date-down`,
+  DATE_UP: `date-up`
+};
+
 const crreateSortTemplate = () => {
   return (
     `<div class="board__filter-list">
-      <a href="#" class="board__filter">SORT BY DEFAULT</a>
-      <a href="#" class="board__filter">SORT BY DATE up</a>
-      <a href="#" class="board__filter">SORT BY DATE down</a>
+      <a href="#" data-sort-type="${SortType.DEFAULT}" class="board__filter">SORT BY DEFAULT</a>
+      <a href="#" data-sort-type="${SortType.DATE_UP}" class="board__filter">SORT BY DATE UP</a>
+      <a href="#" data-sort-type="${SortType.DATE_DOWN}" class="board__filter">SORT BY DATE DOWN</a>
     </div>`
   );
 };
@@ -13,6 +19,26 @@ const crreateSortTemplate = () => {
 class SortComponent extends AbstractComponent {
   getTemplate() {
     return crreateSortTemplate();
+  }
+
+  setSortTypeChengeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      const sortType = evt.target.dataset.sortType;
+
+      if (this._currentSortType === sortType) {
+        return;
+      }
+
+      this._currentSortType = sortType;
+
+      handler(this._currentSortType);
+    });
   }
 }
 export default SortComponent;
