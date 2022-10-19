f6165db9734064253757cd83ae2dd028de05db3d
dsalangin <dsalangin@74.ru>
2022-10-19 15:53:30 +0300

    создал компонент отрисовки пустой доски

---
+++ src/components/no-task.js
@@ -0,0 +1,17 @@
+import AbstractComponent from "./abstract-component";
+
+const createNoTaskTemplate = () => {
+  return (
+    `<p class="board__no-tasks">
+      Click «ADD NEW TASK» in menu to create your first task
+    </p>`
+  );
+};
+
+class NoTaskComponent extends AbstractComponent {
+  getTemplate() {
+    return createNoTaskTemplate();
+  }
+}
+
+export default NoTaskComponent;
