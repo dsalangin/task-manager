263996ef0e9bac1e693e67a9ee645a146d5e29d4
dsalangin <dsalangin@74.ru>
2022-10-20 11:03:19 +0300

    добавил обработчик событий на кнопки архивировать и важное у задачи

--- src/components/task.js
+++ src/components/task.js
@@ -96,6 +96,14 @@ class TaskComponent extends AbstractComponent {
   setEditButtonClickHandler(handler) {
     this.getElement().querySelector(`.card__btn--edit`).addEventListener(`click`, handler);
   }
+
+  setArchiveButtonClickHandler(handler) {
+    this.getElement().querySelector(`.card__btn--archive`).addEventListener(`click`, handler);
+  }
+
+  setArchiveButtonClickHandler(handler) {
+    this.getElement().querySelector(`.card__btn--favorites`).addEventListener(`click`, handler);
+  }
 }
 
 export default TaskComponent;
